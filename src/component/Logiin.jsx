f1e952d6639db0cb6554464549046a1a80c246f0
nutan kumari <nutananju240@gmail.com>
2024-02-13 18:48:27 +0530

    fix yup validation

--- src/component/Logiin.jsx
+++ src/component/Logiin.jsx
@@ -1,35 +1,47 @@
 import React, { useState } from "react";
-import { Formik, Form, Field } from "formik";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { NavLink, useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function Logiin() {
   const [errorMsg, setErrorMsg] = useState('');
   const navigate = useNavigate();
+   
 
-  const handleSubmit = (values) => {
-    if (!values.email || !values.password) {
-      setErrorMsg('Fill in all fields');
-      return;
-    }
-    setErrorMsg('');
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema : Yup.object({
+        email: Yup.string().email().required("Please enter your email"),
+        password: Yup.string().min(6).required("Please enter your password"),
+      }),
+   
+    onSubmit: (values) => {
+      if (!values.email || !values.password) {
+        setErrorMsg('Fill in all fields'); return;
+      }
+      setErrorMsg('');
 
-    signInWithEmailAndPassword(auth, values.email, values.password)
-      .then((res) => {
-        const user = res.user;
-        toast.success('Login successful!');
-        navigate('/home');
-        console.log(res);
-      })
-      .catch(err => {
-        setErrorMsg(err.message);
-        console.log(err.message);
-      });
-  };
+      signInWithEmailAndPassword(auth, values.email, values.password)
+        .then((res) => {
+          const user = res.user;
+          toast.success('Login successful!');
+          navigate('/home');
+          console.log(res);
+        })
+        .catch(err => {
+          setErrorMsg(err.message);
+          console.log(err.message);
+        });
+    },
+  });
 
   return (
     <>
@@ -47,61 +59,76 @@ function Logiin() {
             margin: "auto",
             boxShadow: 3,
             padding: 5,
-            height: 250,
+            height: 290,
             background: "rgb(255 255 255)",
           }}
           className="form_container"
         >
-          <Typography sx={{ fontWeight: "bold" , paddingBottom : '15px' , color: 'rgb(37, 84, 112)' }} variant="h5">
-            Login
+          <Typography sx={{ fontWeight: "bold", paddingBottom: '15px', color: 'rgb(37, 84, 112)' }} variant="h5">
+            Login ...
           </Typography>
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            onSubmit={handleSubmit}
-          >
-            {({ values, handleChange, handleSubmit }) => (
-              <Form onSubmit={handleSubmit}>
-                <Stack
-                  sx={{ width: "100%", fontSize: "19px" }}
-                  direction={"column"}
-                  spacing={1}
-                >
-                  <Field
-                    as={TextField}
-                    fullWidth
-                    label="Email"
-                    type="email"
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                  />
-                  <Field
-                    as={TextField}
-                    fullWidth
-                    label="Password"
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                  />
+          <form onSubmit={formik.handleSubmit}>
+            <Stack
+              sx={{ width: "100%", fontSize: "19px" }}
+              direction={"column"}
+              spacing={1}
+            >
+              <TextField
+                fullWidth
+                label="Email"
+                type="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+               <Typography
+              variant="p"
+              sx={{
+                fontSize: "13px",
+                fontStyle: "italic",
+              }}
+              color={"red"}
+            >
+              {formik.errors.email &&
+                formik.touched.email &&
+                formik.errors.email}
+            </Typography>
+              
+              <TextField
+                fullWidth
+                label="Password"
+                type="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+ <Typography
+              variant="p"
+              sx={{
+                fontSize: "13px",
+                fontStyle: "italic",
+              }}
+              color={"red"}
+            >
+              {formik.errors.password &&
+                formik.touched.password &&
+                formik.errors.password}
+            </Typography>
+              <Button
+                variant="contained"
+                sx={{
+                  background:
+                    "linear-gradient(90.9deg, rgb(3, 195, 195) 0.3%, rgb(37, 84, 112) 87.8%)",
+                }}
+                type="submit"
+              >
+                Login
+              </Button>
+            </Stack>
+          </form>
 
-                  <Button
-                    variant="contained"
-                    sx={{
-                      background:
-                        "linear-gradient(90.9deg, rgb(3, 195, 195) 0.3%, rgb(37, 84, 112) 87.8%)",
-                    }}
-                    type="submit"
-                  >
-                    Login
-                  </Button>
-                </Stack>
-              </Form>
-            )}
-          </Formik>
           <Box sx={{ marginTop: '15px' }}>
             <NavLink
               style={{ color: "#1565c0", paddingTop: 3 }}
--- src/component/Registration.jsx
+++ src/component/Registration.jsx
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, useFormik } from "formik";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { NavLink, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import * as Yup from "yup";
 
 function Registration() {
-  const [errorMsg, setErrorMsg] = useState('');
+  const [errorMsg, setErrorMsg] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (values) => {
-    if (!values.username || !values.email || !values.password) {
-      setErrorMsg('Fill in all fields');
-      return;
-    }
-    setErrorMsg('');
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("Please enter your username"),
+      email: Yup.string().email().required("Please enter your email"),
+      password: Yup.string().min(6).required("Please enter your password"),
+    }),
+    onSubmit: (values) => {
+      if (!values.username || !values.email || !values.password) {
+        setErrorMsg("Fill in all fields");
+        return;
+      }
+      setErrorMsg("");
 
-    createUserWithEmailAndPassword(auth, values.email, values.password)
-      .then((res) => {
-        const user = res.user;
-        toast.success('Registration successful!');
-        navigate('/');
-        console.log(user);
-        console.log(res);
-      })
-      .catch(err => {
-        setErrorMsg(err.message);
-        console.log(err.message);
-      });
-  };
+      createUserWithEmailAndPassword(auth, values.email, values.password)
+        .then((res) => {
+          const user = res.user;
+          toast.success("Registration successful!");
+          navigate("/");
+          console.log(user);
+          console.log(res);
+        })
+        .catch((err) => {
+          setErrorMsg(err.message);
+          console.log(err.message);
+        });
+    },
+  });
 
   return (
     <>
@@ -48,72 +61,113 @@ function Registration() {
             margin: "auto",
             boxShadow: 3,
             padding: 5,
-            height: 300,
+            height: 350,
             background: "rgb(255 255 255)",
           }}
           className="form_container"
         >
-          <Typography sx={{ fontWeight: "bold" , paddingBottom : '15px' }} variant="h5">
+          <Typography
+            sx={{ fontWeight: "bold", paddingBottom: "15px" }}
+            variant="h5"
+          >
             Sign Up
           </Typography>
+
           <Formik
             initialValues={{
               username: "",
               email: "",
               password: "",
             }}
-            onSubmit={handleSubmit}
+            onSubmit={formik.handleSubmit}
           >
-            {({ values, handleChange, handleSubmit }) => (
-              <Form onSubmit={handleSubmit}>
-                <Stack
-                  sx={{ width: "100%", fontSize: "19px" }}
-                  direction={"column"}
-                  spacing={1}
+            <Form>
+              <Stack
+                sx={{ width: "100%", fontSize: "19px" }}
+                direction={"column"}
+                spacing={1}
+              >
+                <Field
+                  as={TextField}
+                  fullWidth
+                  label="Username"
+                  type="text"
+                  name="username"
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <Typography
+                  variant="p"
+                  sx={{
+                    fontSize: "13px",
+                    fontStyle: "italic",
+                  }}
+                  color={"red"}
                 >
-                  <Field
-                    as={TextField}
-                    fullWidth
-                    label="Username"
-                    type="text"
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                  />
-                  <Field
-                    as={TextField}
-                    fullWidth
-                    label="Email"
-                    type="email"
-                    name="email"
-                    value={values.email}
-                    onChange={handleChange}
-                  />
-                  <Field
-                    as={TextField}
-                    fullWidth
-                    label="Password"
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                  />
-
-                  <Button
-                    variant="contained"
-                    sx={{
-                      background:
-                        "linear-gradient(90.9deg, rgb(3, 195, 195) 0.3%, rgb(37, 84, 112) 87.8%)",
-                    }}
-                    type="submit"
-                  >
-                    Sign Up
-                  </Button>
-                </Stack>
-              </Form>
-            )}
+                  {formik.errors.username &&
+                    formik.touched.username &&
+                    formik.errors.username}
+                </Typography>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  label="Email"
+                  type="email"
+                  name="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <Typography
+                  variant="p"
+                  sx={{
+                    fontSize: "13px",
+                    fontStyle: "italic",
+                  }}
+                  color={"red"}
+                >
+                  {formik.errors.email &&
+                    formik.touched.email &&
+                    formik.errors.email}
+                </Typography>
+                <Field
+                  as={TextField}
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  name="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <Typography
+                  variant="p"
+                  sx={{
+                    fontSize: "13px",
+                    fontStyle: "italic",
+                  }}
+                  color={"red"}
+                >
+                  {formik.errors.password &&
+                    formik.touched.password &&
+                    formik.errors.password}
+                </Typography>
+                <Button
+                  variant="contained"
+                  sx={{
+                    background:
+                      "linear-gradient(90.9deg, rgb(3, 195, 195) 0.3%, rgb(37, 84, 112) 87.8%)",
+                  }}
+                  type="submit"
+                >
+                  Sign Up
+                </Button>
+              </Stack>
+            </Form>
           </Formik>
-          <Box  sx={{paddingTop: 3}}>
+
+          <Box sx={{ paddingTop: 3 }}>
             <NavLink
               style={{ color: "#1565c0", paddingTop: 3 }}
               to={"/"}
